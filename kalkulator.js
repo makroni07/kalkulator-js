7b38a020b95485ae2e16ec883cc77363bafe29ee
makroni07 <mnaufal4567@gmail.com>
2018-09-03 16:37:35 +0700

    kalkulator.js

---
+++ kalkulator.js
@@ -0,0 +1,41 @@
+// pertambahan 
+
+var angka1 = 3;
+var angka2 = 5;
+
+console.log(angka2 + angka1);
+
+
+//pengurangan
+
+var angka3 = 23;
+var angka4 = 2;
+
+console.log(angka3 - angka4);
+
+
+//perkalian
+
+var angka7 = 8;
+var angka8 = 3;
+
+console.log(angka7 * angka8);
+
+
+
+//pambagian
+
+var angka5 = 12;
+var angka6 = 6;
+
+console.log(angka5 / angka6);
+
+
+//modulus
+
+var angka9 = 4;
+var angka10 = 2;
+
+console.log(angka9 % angka10);
+
+
