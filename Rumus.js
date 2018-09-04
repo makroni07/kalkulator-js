db17fbeab3c7223e78f93e4a2bdf898da3cef2f1
makroni07 <mnaufal4567@gmail.com>
2018-09-04 10:58:10 +0700

    Belajar kalkulator Rumus

---
+++ Rumus.js
@@ -0,0 +1,38 @@
+// luas persegi
+var a = prompt("masukan nilai panjang");
+var b = prompt("masukan nilai lebar");
+var luas = a * b;
+
+console.log("luas persegi panjang = "+luas+" cm2");
+
+
+// luas segitiga
+var a = prompt("masukan nilai alas");
+var b = prompt("masukan nilai tinggi");
+var luas = 0.5 * a * b;
+
+console.log("luas segitiga = "+luas+" cm2");
+
+
+// luas jajar genjang
+var a = prompt("masukan nilai alas");
+var b = prompt("masukan nilai tinggi");
+var luas = a * b
+
+console.log("luas jajar genjang = "+luas+" cm2");
+
+
+// volume kubus
+var a = prompt("masukan nilai sisi");
+var volume = a * a * a;
+
+console.log("volume kubus = "+volume+" cm3");
+
+
+// volume tabung
+var a = prompt("masukan nilai jari-jari");
+var b = prompt("masukan nilai tinggi");
+var Pi = 22 / 7;
+var volume = Pi * a * a * b;
+
+console.log("volume tabung = "+volume+" cm3");
